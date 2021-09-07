a06c9720e69db933f6456536a61c6818066edb76
shyeonnnny <ktmdgus970807@gmail.com>
2021-09-07 10:09:07 +0900

    first commit

---
+++ practice01/src/main/java/prob2/Prob2.java
@@ -0,0 +1,13 @@
+package prob2;
+
+public class Prob2 {
+	public static void main(String[] args) {	
+		for(int i=1; i<=10; i++) {
+			for (int a=0; a<=9; a++) {
+				System.out.print((i+a) + " ");
+			}
+			System.out.println();
+			
+		}
+	}
+}
