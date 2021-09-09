94bffd4900297aa3c87c03cacf304ff716085519
shyeonnnny <ktmdgus970807@gmail.com>
2021-09-09 18:03:00 +0900

    2021.09.09

--- project02/src/main/java/prob05/Prob05.java
+++ project02/src/main/java/prob05/Prob05.java
@@ -9,23 +9,53 @@ public class Prob05 {
 		Scanner scanner = new Scanner( System.in );
 
 		while( true ) {
-			
+
 			/* 게임 작성 */
 
 			// 정답 램덤하게 만들기
+			int count = 0;
 			Random random = new Random();
 			int correctNumber = random.nextInt( 100 ) + 1;
-			System.out.println(correctNumber);
-
 			
+			System.out.println("수를 결정하였습니다. 맞추어 보세요");
+			System.out.println("1-100");
+
+			while ( true ) {
+				
+				count++;
+
+				System.out.println(count + ">>");
+				int select_number = scanner.nextInt();
+				
+				if (select_number > 100 || select_number < 0) {
+					System.out.println("1-100사이 숫자를 선택하세요.");
+					
+				}
+
+				if (select_number > correctNumber) {
+					System.out.println("더 낮게");
+					continue;
+				}else if ( select_number < correctNumber){
+					System.out.println("더 높게");
+					continue;
+				}else if (select_number == correctNumber) {
+					System.out.println("맞았습니다.");
+					break;
+
+				}
+				
+
+			}
+
+
 			//새 게임 여부 확인하기
-			System.out.print( "다시 하겠습니까(y/n)>>" );
+			System.out.print( "다시하시겠습니까(y/n)>>" );
 			String answer = scanner.next();
 			if( "y".equals( answer ) == false ) {
 				break;
 			}
 		}
-		
+
 		scanner.close();
 	}
 
