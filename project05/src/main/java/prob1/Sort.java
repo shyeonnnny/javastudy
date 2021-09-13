2bac490da319ac42274eae281660f28c00ad36f5
shyeonnnny <ktmdgus970807@gmail.com>
2021-09-13 20:19:49 +0900

    2021.09.13

---
+++ project05/src/main/java/prob1/Sort.java
@@ -0,0 +1,38 @@
+package prob1;
+
+public class Sort {
+
+	public static void main(String[] arg) {
+
+		int array[] = { 5, 9, 3, 8, 60, 20, 1 };
+		int count =  array.length;
+
+		System.out.println( "Before sort." );
+
+		for (int i = 0; i < count; i++) {
+			System.out.print( array[ i ] + " " );
+		}
+
+		//
+		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
+		//
+		for (int j=0; j<count; j++) {
+			for (int i=0; i<count-1; i++) {
+				if (array[i] < array[i+1]) {
+					int a = array[i];
+					array[i] = array[i+1];
+					array[i+1] = a;
+				} else {
+					continue;
+				}
+			}}
+
+
+		// 결과 출력
+		System.out.println( "\nAfter Sort." );
+
+		for (int i = 0; i < count; i++) {
+			System.out.print(array[i] + " ");
+		}		
+	}
+}
---
+++ project05/src/main/java/prob3/Bird.java
@@ -0,0 +1,21 @@
+package prob3;
+
+public abstract class Bird {
+	protected String name;
+	
+	public void fly() {
+		
+	};
+	
+	public void sing() {
+		
+	};
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
